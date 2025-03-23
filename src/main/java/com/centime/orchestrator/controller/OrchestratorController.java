2fe18acf463a60f213b3c863564a92afe356208b
sorov0 <saurav96.hzb@gmail.com>
2025-03-23 17:07:57 +0530

    Code pushed

---
+++ src/main/java/com/centime/orchestrator/controller/OrchestratorController.java
@@ -0,0 +1,46 @@
+package com.centime.orchestrator.controller;
+
+import com.centime.orchestrator.dto.PersonRequest;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/orchestrator")
+@RequiredArgsConstructor
+public class OrchestratorController {
+
+    @Autowired
+    private final RestTemplate restTemplate;
+
+    private final Logger logger = LoggerFactory.getLogger(OrchestratorController.class);
+
+    @GetMapping("/health")
+    public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok("Up");
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processRequest(@RequestBody PersonRequest request) {
+
+        String traceId = UUID.randomUUID().toString();
+        logger.info("[{}] Received request: {}", traceId, request);
+
+        // Call Service 2 (GET)
+        ResponseEntity<String> greetResponse = restTemplate.getForEntity("http://localhost:8081/greet", String.class);
+
+        // Call Service 3 (POST)
+        ResponseEntity<String> concatResponse = restTemplate.postForEntity("http://localhost:8082/concat", request, String.class);
+
+        String finalResponse = greetResponse.getBody() + " " + concatResponse.getBody();
+        logger.info("[{}] Final response: {}", traceId, finalResponse);
+
+        return ResponseEntity.ok(finalResponse);
+    }
+}
---
+++ src/main/java/com/centime/orchestrator/dto/PersonRequest.java
@@ -0,0 +1,13 @@
+package com.centime.orchestrator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonRequest {
+    private String name;
+    private String surname;
+}
